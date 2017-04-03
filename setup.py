43d125b475893323e406f615d200e5abffa161eb
Florian Thoele <florian.thoele@mat.ethz.ch>
2017-04-03 13:56:35 +0200

    first try at a setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name = "tensormoments",
+    version = "0.0.1",
+    author = "Florian Thoele",
+    author_email = "florian.thoele@gmail.com",
+    description = ("Helper functions to deal with output files containing tensor moments"),
+    license = "MIT",
+    keywords = "",
+    url = "",
+    packages=['tensormoments'],
+    long_description=""
+    )
