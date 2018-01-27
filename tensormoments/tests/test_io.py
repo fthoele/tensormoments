fa2040dd551c7aa078be02ef482e526d37b0de8e
Florian Thoele <florian.thoele@mat.ethz.ch>
2018-01-27 21:29:49 +0100

    changing argument name in test

--- tensormoments/tests/test_io.py
+++ tensormoments/tests/test_io.py
@@ -8,8 +8,7 @@ def this_dir():
     return os.path.dirname(os.path.realpath(__file__))
 
 
-def test_readElkOutput(needs_io):
+def test_readElkOutput(this_dir):
     filename = os.path.join(this_dir, 'test_data/test_1_small.txt')
     d = io.readElkOutput(filename)
     assert len(d['value']) == 16
-    
